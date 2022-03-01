771780092481a1448fd84007c45ab27875346aa5
WhalesBob <ndw8200@naver.com>
2022-03-01 18:45:33 +0900

    DS_03 question information update

--- DS_03/2/2.c
+++ DS_03/2/2.c
@@ -1,3 +1,6 @@
+// 입력 받은 이차원 배열의 행의 수와 열의 수를 함수 make2DArray()에 전달하면, 함수 make2DArray()는 전달 받은 원소 갯수만큼
+// 동적으로 메모리를 할당 받아 랜덤으로 100보다 작은 값으로 메모리를 초기화시킨 후, 주소값을 반환한다. 
+
 #define MALLOC(p,s){\
 	if(!((p) = malloc(s))){\
 		fprintf(stderr,"Insufficient Memory");\
